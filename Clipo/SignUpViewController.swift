5f6f6d8dd2e1b4dbb8498e49d3e4772d01adf18e
tony056 <tony61507@gmail.com>
2016-03-26 01:05:35 +0800

    Refactor some methods

--- Clipo/SignUpViewController.swift
+++ Clipo/SignUpViewController.swift
@@ -7,18 +7,54 @@
 //
 
 import UIKit
+import Material
 
 class SignUpViewController: UIViewController {
 
+
+    
+    @IBOutlet weak var emailTextField: TextField!
+    @IBOutlet weak var userNameTextField: TextField!
+    @IBOutlet weak var passwordTextField: TextField!
+    @IBOutlet weak var signUpBtn: FlatButton!
+    @IBOutlet weak var cancelBtn: FlatButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupSignUpView()
+        self.hideKeyBoardWhenTapped()
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
         // Dispose of any resources that can be recreated.
+    }
+    
+    func setupSignUpView(){
+        self.setupTextFieldView(self.emailTextField, placeholder: "Email:")
+        self.setupTextFieldView(self.userNameTextField, placeholder: "User Name:")
+        self.passwordTextField.secureTextEntry = true
+        self.setupTextFieldView(self.passwordTextField, placeholder: "Password:")
+        
+        self.setupBtnView(self.signUpBtn, text: "Sign Up")
+        self.setupBtnView(self.cancelBtn, text: "Cancel")
+    }
+    
+    func setupTextFieldView(target: TextField, placeholder: String){
+        target.placeholder = placeholder
+        target.placeholderTextColor = MaterialColor.grey.base
+        target.textColor = MaterialColor.black
+        target.titleLabel = UILabel()
+        target.titleLabelColor = MaterialColor.grey.base
+        target.titleLabelActiveColor = MaterialColor.red.base
+    }
+    
+    func setupBtnView(target: FlatButton, text: String){
+        target.setTitle(text, forState: .Normal)
+        target.setTitleColor(MaterialColor.red.base, forState: .Normal)
     }
     
 
